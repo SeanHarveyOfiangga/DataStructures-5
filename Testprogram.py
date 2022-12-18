c502b2741ed3ce0aa4aee7b496c272d46f42671b
unknown <seanharveyofiangga@gmail.com>
2022-12-18 14:39:06 +0800

    Finished testing the fourth example

--- Testprogram.py
+++ Testprogram.py
@@ -30,3 +30,14 @@ print("\nThird Example")
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
     print(element)
+
+#fourth example
+lifo = []
+print("\nFourth Example")
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
